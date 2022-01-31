ac53876bb469531239888d9fe7105345276dbebf
Chase Stokes <chasehunterstokes@gmail.com>
2022-01-31 11:06:13 -0800

    install jest and add test file

--- src/triangle.js
+++ src/triangle.js
@@ -2,8 +2,11 @@ export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
+  this.isTriangle = ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side))) ? "not a triangle" : "this is a triangle";
+  this.isScalene = ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) ? "is scalene" : "not scalene";
+  this.isEquilateral = ((this.side1 === this.side2) && (this.side1 === this.side3)) ? "is equilateral" : "not equilateral";
 }
 
 Triangle.prototype.checkType = function() {
-  return "I can't answer that yet!";
+  return "Nani! Not a triangle!";
 };
